c0a80a333964eb03909d91ed37138483a520156a
Alex Saltstein <Alex@alexs-macbook-pro.local>
2018-07-29 16:49:12 -0400

    Initial Commit

---
+++ BeerBuds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeerBuds
+//
+//  Created by Alex Saltstein on 7/29/18.
+//  Copyright © 2018 Alex Saltstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
