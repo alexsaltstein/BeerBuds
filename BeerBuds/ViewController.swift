02eac06527f15bdbc4a903a0cebe5c813cb45fa6
Alex Saltstein <Alex@Alexs-MacBook-Pro.local>
2018-07-30 13:37:52 -0400

    added loading screen

--- BeerBuds/ViewController.swift
+++ BeerBuds/ViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet var loadingView: UIView!
+    @IBOutlet weak var beerView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        showLoadingScreen()
+        
+        //area where data is loaded into app
+        
+        
     }
 
+    func showLoadingScreen(){
+        loadingView.bounds.size.width = view.bounds.width
+        loadingView.bounds.size.height = view.bounds.height
+        loadingView.center = view.center
+        loadingView.alpha = 0
+        beerView.frame = CGRect(x: 0, y: view.bounds.height, width: view.bounds.width, height: view.bounds.height)
+        view.addSubview(loadingView)
+        UIView.animate(withDuration: 0.3, delay: 0.5, options: [], animations: {
+            self.loadingView.alpha = 1
+        }) { (success) in
+            self.animateBeerMovement()
+        }
+    }
+    
+    func animateBeerMovement(){
+        let height = view.bounds.height * -1
+        UIView.animate(withDuration: 3, delay: 0.2, options: [], animations: {
+            self.beerView.transform = CGAffineTransform(translationX: 0, y: height)
+        }, completion: { (success) in
+            self.hideLoadingScreen()
+        })
+    }
+    
+    func hideLoadingScreen(){
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: [], animations: { 
+            self.loadingView.alpha = 0
+        })
+    }
+    
+    func getData(){
+        //temporary function to just represent a delay of getting data
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
